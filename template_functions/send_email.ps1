55bb84e960966db6b55ef3054d48e4613cdb3593
Jadin Heaston <jadin.heaston@como.gov>
2025-06-24 09:43:37 -0500

    Adding .env support for basic types.
    Signed-off-by: Jadin Heaston <jadin.heaston@como.gov>

---
+++ template_functions/send_email.ps1
@@ -0,0 +1,42 @@
+function Send-Email {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true)]
+		[string[]]$Destination,
+
+		[Parameter(Mandatory = $true)]
+		[string]$Subject,
+
+		[Parameter(Mandatory = $true)]
+		[string]$HTMLBody,
+
+		[string[]]$CC,
+		[string[]]$BCC,
+		[string]$TextBody,
+		[string[]]$AttachmentList,
+		[PSCredential]$Credential
+	)
+
+	# Ensure Send-MailKitMessage module is available
+	if (-not (Get-Module -ListAvailable -Name Send-MailKitMessage)) {
+		Install-Module -Name Send-MailKitMessage -Force
+	}
+
+	# Important if the certificate is self-signed or causing problems.
+	if ($false -eq ([bool]$Env:SMTP_VALIDATE_CERTIFICATE)) {
+		[System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
+	}
+
+	Send-MailKitMessage -SMTPServer ([string]$Env:SMTP_SERVER) `
+		-Port ([int]$Env:SMTP_PORT) `
+		-From ([string]$Env:SMTP_FROM) `
+		-RecipientList $Destination `
+		-CCList $CC `
+		-BCCList $BCC `
+		-Subject $Subject `
+		-TextBody $TextBody `
+		-HTMLBody $HTMLBody `
+		-AttachmentList $AttachmentList `
+		-Credential $Credential `
+		-UseSecureConnectionIfAvailable
+}
